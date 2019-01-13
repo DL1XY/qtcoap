75c82a376e84732e3014584df43812bb5d4beedc
Arne Wörheide <arnewo@hotmail.com>
2019-01-13 12:03:45 +0100

    first commit

---
+++ tests/build/build-tests-Debug/moc_coaptests.cpp
@@ -0,0 +1,218 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'coaptests.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../coaptests.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'coaptests.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_CoapTests_t {
+    QByteArrayData data[27];
+    char stringdata0[313];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CoapTests_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CoapTests_t qt_meta_stringdata_CoapTests = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "CoapTests"
+QT_MOC_LITERAL(1, 10, 15), // "observeResource"
+QT_MOC_LITERAL(2, 26, 0), // ""
+QT_MOC_LITERAL(3, 27, 20), // "observeLargeResource"
+QT_MOC_LITERAL(4, 48, 15), // "invalidUrl_data"
+QT_MOC_LITERAL(5, 64, 10), // "invalidUrl"
+QT_MOC_LITERAL(6, 75, 13), // "invalidScheme"
+QT_MOC_LITERAL(7, 89, 4), // "ping"
+QT_MOC_LITERAL(8, 94, 5), // "hello"
+QT_MOC_LITERAL(9, 100, 6), // "broken"
+QT_MOC_LITERAL(10, 107, 5), // "query"
+QT_MOC_LITERAL(11, 113, 7), // "subPath"
+QT_MOC_LITERAL(12, 121, 20), // "extendedOptionLength"
+QT_MOC_LITERAL(13, 142, 17), // "specialCharacters"
+QT_MOC_LITERAL(14, 160, 18), // "extendedDelta_data"
+QT_MOC_LITERAL(15, 179, 13), // "extendedDelta"
+QT_MOC_LITERAL(16, 193, 6), // "secret"
+QT_MOC_LITERAL(17, 200, 9), // "separated"
+QT_MOC_LITERAL(18, 210, 14), // "deleteResource"
+QT_MOC_LITERAL(19, 225, 4), // "post"
+QT_MOC_LITERAL(20, 230, 3), // "put"
+QT_MOC_LITERAL(21, 234, 11), // "jsonMessage"
+QT_MOC_LITERAL(22, 246, 13), // "largeDownload"
+QT_MOC_LITERAL(23, 260, 11), // "largeCreate"
+QT_MOC_LITERAL(24, 272, 11), // "largeUpdate"
+QT_MOC_LITERAL(25, 284, 13), // "multipleCalls"
+QT_MOC_LITERAL(26, 298, 14) // "coreLinkParser"
+
+    },
+    "CoapTests\0observeResource\0\0"
+    "observeLargeResource\0invalidUrl_data\0"
+    "invalidUrl\0invalidScheme\0ping\0hello\0"
+    "broken\0query\0subPath\0extendedOptionLength\0"
+    "specialCharacters\0extendedDelta_data\0"
+    "extendedDelta\0secret\0separated\0"
+    "deleteResource\0post\0put\0jsonMessage\0"
+    "largeDownload\0largeCreate\0largeUpdate\0"
+    "multipleCalls\0coreLinkParser"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CoapTests[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      25,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  139,    2, 0x08 /* Private */,
+       3,    0,  140,    2, 0x08 /* Private */,
+       4,    0,  141,    2, 0x08 /* Private */,
+       5,    0,  142,    2, 0x08 /* Private */,
+       6,    0,  143,    2, 0x08 /* Private */,
+       7,    0,  144,    2, 0x08 /* Private */,
+       8,    0,  145,    2, 0x08 /* Private */,
+       9,    0,  146,    2, 0x08 /* Private */,
+      10,    0,  147,    2, 0x08 /* Private */,
+      11,    0,  148,    2, 0x08 /* Private */,
+      12,    0,  149,    2, 0x08 /* Private */,
+      13,    0,  150,    2, 0x08 /* Private */,
+      14,    0,  151,    2, 0x08 /* Private */,
+      15,    0,  152,    2, 0x08 /* Private */,
+      16,    0,  153,    2, 0x08 /* Private */,
+      17,    0,  154,    2, 0x08 /* Private */,
+      18,    0,  155,    2, 0x08 /* Private */,
+      19,    0,  156,    2, 0x08 /* Private */,
+      20,    0,  157,    2, 0x08 /* Private */,
+      21,    0,  158,    2, 0x08 /* Private */,
+      22,    0,  159,    2, 0x08 /* Private */,
+      23,    0,  160,    2, 0x08 /* Private */,
+      24,    0,  161,    2, 0x08 /* Private */,
+      25,    0,  162,    2, 0x08 /* Private */,
+      26,    0,  163,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void CoapTests::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        CoapTests *_t = static_cast<CoapTests *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->observeResource(); break;
+        case 1: _t->observeLargeResource(); break;
+        case 2: _t->invalidUrl_data(); break;
+        case 3: _t->invalidUrl(); break;
+        case 4: _t->invalidScheme(); break;
+        case 5: _t->ping(); break;
+        case 6: _t->hello(); break;
+        case 7: _t->broken(); break;
+        case 8: _t->query(); break;
+        case 9: _t->subPath(); break;
+        case 10: _t->extendedOptionLength(); break;
+        case 11: _t->specialCharacters(); break;
+        case 12: _t->extendedDelta_data(); break;
+        case 13: _t->extendedDelta(); break;
+        case 14: _t->secret(); break;
+        case 15: _t->separated(); break;
+        case 16: _t->deleteResource(); break;
+        case 17: _t->post(); break;
+        case 18: _t->put(); break;
+        case 19: _t->jsonMessage(); break;
+        case 20: _t->largeDownload(); break;
+        case 21: _t->largeCreate(); break;
+        case 22: _t->largeUpdate(); break;
+        case 23: _t->multipleCalls(); break;
+        case 24: _t->coreLinkParser(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject CoapTests::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_CoapTests.data,
+      qt_meta_data_CoapTests,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *CoapTests::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CoapTests::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CoapTests.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int CoapTests::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 25)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 25;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 25)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 25;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
